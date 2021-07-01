0735bfb1d80d6dac13d77f05a2b43f1f8f141f0f
Yoann <depriester.yoann@neuf.fr>
2021-07-01 16:24:51 +0200

    Commit 01/07/2121 - Création Appli Vide + Google Map
    - Mise en place de Google Map
    - Accès Google Map depuis Accueil via Menu
    - Accès Accueil depuis Google Map via Menu

--- app/src/main/java/com/pokemongo/flagcatcher/MainActivity.kt
+++ app/src/main/java/com/pokemongo/flagcatcher/MainActivity.kt
@@ -2,10 +2,28 @@ package com.pokemongo.flagcatcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu?.add(0,1,0,"Map")
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId ==1){
+                val intent=Intent(this, MapsActivity::class.java)
+                startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
+
+
