fa5bb1be32ee70724eb72f77cee253d5e3ef57e9
Ephyekas <80513723+Ephyekas@users.noreply.github.com>
2021-07-05 14:57:00 +0200

    ajout de parametre (bean client)

--- Flag Catcher/app/src/main/java/com/pokemongo/flagcatcher/model/beans/CoordinateBean.kt
+++ Flag Catcher/app/src/main/java/com/pokemongo/flagcatcher/model/beans/CoordinateBean.kt
@@ -1,4 +1,8 @@
 package com.pokemongo.flagcatcher.model.beans
 
-class CoordinateBean {
+data class CoordinateBean ( val id_coordinate: Int,  val long_coordinate: Int, val lat_coordinate: Int){
+
+
+
+
 }
