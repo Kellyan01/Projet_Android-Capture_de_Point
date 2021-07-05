56c4ac8708f48cc0a3771c997e64c14e3cc2b8c0
Ephyekas <80513723+Ephyekas@users.noreply.github.com>
2021-07-05 16:56:42 +0200

    ajout de sendGetOkHttpRequest ( OkHttpUtils )

--- Flag Catcher/app/src/main/java/com/pokemongo/flagcatcher/model/utils/OkHttpUtils.kt
+++ Flag Catcher/app/src/main/java/com/pokemongo/flagcatcher/model/utils/OkHttpUtils.kt
@@ -1,4 +1,29 @@
 package com.pokemongo.flagcatcher.model.utils
 
-class OkHttpUtils {
+
+import android.util.Log
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+object OkhttpUtils {
+    @Throws(Exception::class)
+    fun sendGetOkHttpRequest(url: String): String {
+        Log.w("tag", "url : $url")
+        val client = OkHttpClient()
+
+        //Création de la requete
+        val request= Request.Builder().url(url).build()
+
+        //Execution de la requête
+        val response = client.newCall(request).execute()
+
+        //Analyse du code retour
+        return if (response.code < 200 || response.code >= 300) {
+            throw Exception("Réponse du serveur incorrect : " + response.code)
+        } else {
+
+            //Résultat de la requete.
+            response.body!!.string()
+        }
+    }
 }
--- Flag Catcher/app/src/main/java/com/pokemongo/flagcatcher/model/utils/WSUtils.kt
+++ Flag Catcher/app/src/main/java/com/pokemongo/flagcatcher/model/utils/WSUtils.kt
@@ -1,4 +1,7 @@
 package com.pokemongo.flagcatcher.model.utils
 
+import com.google.android.gms.maps.model.LatLng
+
 class WSUtils {
+
 }
