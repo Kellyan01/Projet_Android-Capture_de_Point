d456974b5c8b5713cf32a9eb982bd20cd80b4ac6
Ephyekas <80513723+Ephyekas@users.noreply.github.com>
2021-07-06 09:42:10 +0200

    Ajout sendPost (OkHttpUtils)

--- Flag Catcher/app/src/main/java/com/pokemongo/flagcatcher/model/utils/OkHttpUtils.kt
+++ Flag Catcher/app/src/main/java/com/pokemongo/flagcatcher/model/utils/OkHttpUtils.kt
@@ -26,4 +26,30 @@ object OkhttpUtils {
             response.body!!.string()
         }
     }
+    @Throws(Exception::class)
+    fun sendPostOkHttpRequest(url: String, paramJson: String?): String {
+        Log.w("tag", "url: $url")
+        OkHttpClientclient = newOkHttpClient()
+        MediaTypeJSON = MediaType.parse("application/json; charset=utf-8")
+
+        //Corps de la requête
+        RequestBodybody = RequestBody.create(JSON, paramJson)
+
+        //Création de la requete
+        val request: Request = newRequest.Builder().url(url).post(body).build()
+
+        //Executionde la requête
+        Responseresponse= client.newCall(request).execute();
+
+        //Analyse du code retour
+        if (response.code() < 200 || response.code() >= 300) {
+            thrownewException("Réponse du serveur incorrect : " + response.code())
+        }
+
+        else {
+
+            //Résultat de la requete.
+            return response.body().string()
+        }
+    }
 }
