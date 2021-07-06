e5359cc4542e745dba672faac57d91006fa92a38
anael182 <anael.bouchema@gmail.com>
2021-07-06 13:48:29 +0200

    WebserviceV2

--- pokemongoServer/src/main/kotlin/com/pokemongo/pokemongo/CoordinateControler.kt
+++ pokemongoServer/src/main/kotlin/com/pokemongo/pokemongo/CoordinateControler.kt
@@ -1,7 +1,6 @@
 package com.pokemongo.pokemongo
 
 import CoordinateBean
-import com.google.gson.Gson
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
